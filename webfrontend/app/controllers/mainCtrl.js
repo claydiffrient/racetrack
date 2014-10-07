cb7f0653829578f07af131d215889e9789b7d0b8
Clay Diffrient <cdiffrient@instructure.com>
2014-10-07 17:03:03 -0600

    Last commit prior to ripping out angular-google-maps

--- webfrontend/app/controllers/mainCtrl.js
+++ webfrontend/app/controllers/mainCtrl.js
@@ -4,14 +4,46 @@ var app = angular.module('RaceTrack');
 
 app.controller('MainCtrl', ['$scope', 'CoordService', function ($scope, CoordService) {
 
+  $scope.bikerLine = {};
+  $scope.bikerLine.path = [{
+    latitude: 40.24102703671923,
+    longitude: -111.65750456984306
+  },{
+    latitude: 40.24102577071256,
+    longitude: -111.65742266108956
+  }];
 
 
   $scope.map = {
       center: CoordService.getMapCoordinates(),
-      zoom: 8
+      zoom: 15
   };
 
-  $scope.data = CoordService.getData();
-  console.log($scope.data);
+  $scope.bikerLine = {
+    path: [],
+    stroke: {
+      color: '#6060FB',
+      weight: 3
+    },
+    editable: false,
+    draggable: true,
+    geodesic: true,
+    visible: true,
+    icons: [{
+        icon: {
+            path: google.maps.SymbolPath.BACKWARD_OPEN_ARROW
+        },
+        offset: '25px',
+        repeat: '50px'
+    }]
+  };
 
-  }]);
+  var unwatch = CoordService.getData().$watch(function() {
+    var coordinates = _.pluck(CoordService.getData(), 'coords');
+    $scope.bikerLine.path.push(_.last(coordinates));  // Push the last into our line.
+    console.log($scope.bikerLine.path);
+  });
+  //CoordService.getData().$bindTo($scope, 'bikerLine');
+
+
+}]);
--- webfrontend/app/services/coordinateService.js
+++ webfrontend/app/services/coordinateService.js
@@ -2,31 +2,38 @@ var app = angular.module('RaceTrack');
 
 app.factory('CoordService', ['$http', '$firebase', function ($http, $firebase) {
 
-  var firebaseRef = new Firebase("https://racetrack.firebaseio.com/bikerGPS");
+  var bikerFirebase = new Firebase("https://racetrack.firebaseio.com/bikerGPS");
+  var droneFirebase = new Firebase("https://racetrack.firebaseio.com/droneGPS");
 
-  var sync = $firebase(firebaseRef);
+  var bikerSync = $firebase(bikerFirebase.limit(100));
+  var droneSync = $firebase(droneFirebase);
+
+  //var bikerArray = bikerSync.$asArray();
+
+
 
   // Get the coordinates for the center of the map.
   var getMapCoordinates = function () {
     return {
-      latitude: 10,
-      longitude: 10
-    };
+      latitude: 40.24,
+      longitude: -111.65
+    }; //_.last(bikerArray);
   };
 
   // Get an array of coordinates of where the biker has been.
   // Used to make the line.
-  var getCoordinatesForLine = function () {
-
+  var getBikerLine = function () {
+    return _.pluck(bikerSync.$asArray(), 'coords');
   };
 
   var getData = function () {
-    return sync.$asObject();
+    return bikerSync.$asArray();
   }
 
 
   return {
     getMapCoordinates: getMapCoordinates,
+    getBikerLine: getBikerLine,
     getData: getData
   };
 
